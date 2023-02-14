8c05887e498e5af1160eef3ed92fbcc6e3f81d60
Jonah Park <jonahp1234@gmail.com>
2023-02-14 04:45:28 +0000

    Jonah Park Issue 1: User can create an entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  var jsonData = JSON.stringify(data);
+  this.localStorage.setItem('jsonData', jsonData);
+});
+
+if (localStorage.getItem('jsonData') === JSON.stringify(data)) {
+  data = JSON.parse(JSON.stringify(data));
+}
--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ var $placeHolderImg = document.querySelector('#placeholder');
 $userPhotoUrl.addEventListener('input', function (event) {
   $placeHolderImg.setAttribute('src', event.target.value);
 });
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formValueObject = {};
+
+  var titleKey = event.target.elements[0];
+  var photoUrlKey = event.target.elements[1];
+  var notesKey = event.target.elements[2];
+
+  formValueObject.titleKey = titleKey.value;
+  formValueObject.photoUrlKey = photoUrlKey.value;
+  formValueObject.notesKey = notesKey.value;
+
+  formValueObject.entryID = data.nextEntryId;
+  data.nextEntryId++;
+
+  data.entries.unshift(formValueObject);
+
+  $placeHolderImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  event.target.reset();
+});
