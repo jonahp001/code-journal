3b989952293c324b634770fb2f9a5e0a52524e0e
Jonah Park <jonahp1234@gmail.com>
2023-02-14 22:07:12 +0000

    Jonah Park issue 2 step 6:  Remove the example HTML entries from index.html once the user's entries are successfully being appended to the DOM on page load and replace them with a message indicating that no entries have been recorded which matches the figma.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,34 @@ $form.addEventListener('submit', function (event) {
 
   event.target.reset();
 });
+
+function renderEntry(entry) {
+  var $newLiElement = document.createElement('li');
+  $newLiElement.setAttribute('class', 'row');
+
+  var $entryPhoto = document.createElement('img');
+  $entryPhoto.setAttribute('src', entry.photoUrlKey);
+  $entryPhoto.setAttribute('class', 'column-half');
+  $newLiElement.appendChild($entryPhoto);
+
+  var $entryTextDiv = document.createElement('div');
+  $entryTextDiv.setAttribute('class', 'column-half');
+  $newLiElement.appendChild($entryTextDiv);
+
+  var $entryTitle = document.createElement('h3');
+  $entryTitle.textContent = entry.titleKey;
+  $entryTextDiv.appendChild($entryTitle);
+
+  var $entryNotes = document.createElement('p');
+  $entryNotes.textContent = entry.notesKey;
+  $entryTextDiv.appendChild($entryNotes);
+
+  return $newLiElement;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var $ulElement = document.querySelector('ul');
+    $ulElement.appendChild(renderEntry(data.entries[i]));
+  }
+});
